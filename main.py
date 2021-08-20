31a6354f11b400eb2e23a2ba68970f96948f9562
TAY JING <tay_jing_yi_deborah@connect.ite.edu.sg>
2021-08-20 22:46:28 +0800

    hello

--- main.py
+++ main.py
@@ -20,107 +20,66 @@ class EuropeStatistics:
         # self.dataFrame = self.dataFrame.assign(year=self.splits)
         self.whatiwant = self.dataFrame[(self.dataFrame["year"].astype(int) >= 1978) & (self.dataFrame["year"].astype(int) <= 1987)]
 
-
-    def TopThreeCountriesMean(self):
-        self.filter1 = self.dataFrame[26: 37]["United Kingdom"].mean()
-        #print(self.filter1)
-
-        self.filter2 = self.dataFrame[26: 37]["Germany"].mean()
-        #print(self.filter2)
-
-        self.filter3 = self.dataFrame[26: 37]["Netherlands"].mean()
-        #print(self.filter3)
-
-        print(f'Mean for the United Kingdom is: {self.filter1}.')
-        print(f'Mean for the Germany is: {self.filter2}.')
-        print(f'Mean for the Netherlands is: {self.filter3}.')
-
-        # hello
-
-
-
-    def TotalVisitorUnitedKingdom(self):
-        self.filter7 = self.dataFrame[26: 37]["United Kingdom"].sum()
-        print(self.filter7)
-        return self.dataFrame[26: 37]["United Kingdom"].sum()
-
-
-
-    def TopThreeCountriesMedian(self):
-        self.filter4 = self.dataFrame[26: 37]["United Kingdom"].median()
-        #print(self.filter4)
-
-        self.filter5 = self.dataFrame[26: 37]["Germany"].median()
-        #print(self.filter5)
-
-        self.filter6 = self.dataFrame[26: 37]["Netherlands"].median()
-        #print(self.filter6)
-
-        print(f'Median for the United Kingdom is: {self.filter4}.')
-        print(f'Median for the Germany is: {self.filter5}.')
-        print(f'Median for the Netherlands is: {self.filter6}.')
-
     def TopThreeCountriesSum(self):
-        self.filter7 = self.dataFrame[26: 37]["United Kingdom"].sum()
-        #print(self.filter7)
+        self.filter7 = self.dataFrame[2: 121]["United Kingdom"].sum()
+        self.filter8 = self.dataFrame[2: 121]["Germany"].sum()
+        self.filter9 = self.dataFrame[2: 121]["France"].sum()
+        self.filter10 = self.dataFrame[2: 121]["Italy"].sum()
+        self.filter11 = self.dataFrame[2: 121]["Netherlands"].sum()
+        self.filter12 = self.dataFrame[2: 121]["Greece"].sum()
+        self.filter13 = self.dataFrame[2: 121]["Belgium & Luxembourg"].sum()
+        self.filter14 = self.dataFrame[2: 121]["Switzerland"].sum()
+        self.filter15 = self.dataFrame[2: 121]["Austria"].sum()
+        self.filter16 = self.dataFrame[2: 121]["Scandinavia"].sum()
+        self.filter17 = self.dataFrame[2: 121]["CIS & Eastern Europe"].sum()
 
-        self.filter8 = self.dataFrame[26: 37]["Germany"].sum()
-        #print(self.filter8)
-
-        self.filter9 = self.dataFrame[26: 37]["Netherlands"].sum()
-        #print(self.filter9)
+        # print(self.filter7)
+        # print(self.filter8)
+        # print(self.filter9)
+        # print(self.filter10)
+        # print(self.filter11)
+        # print(self.filter12)
+        # print(self.filter13)
+        # print(self.filter14)
+        # print(self.filter15)
+        # print(self.filter16)
+        # print(self.filter17)
 
         print(f'The top 3 countries in Europe that visitors visit are United Kingdom {self.filter7}, Germany {self.filter8}, Netherlands {self.filter9}')
 
 
-    # def sumCountries(self):
-    #
-    #     self.sum1 = self.dataFrame["United Kingdom"].sum()
-    #     self.sum2 = self.dataFrame["Germany"].sum()
-    #     self.sum3 = self.dataFrame["France"].sum()
-    #     self.sum4 = self.dataFrame["Italy"].sum()
-    #     self.sum5 = self.dataFrame["Netherlands"].sum()
-    #     self.sum6 = self.dataFrame["Greece"].sum()
-    #     self.sum7 = self.dataFrame["Belgium & Luxembourg"].sum()
-    #     self.sum8 = self.dataFrame["Switzerland"].sum()
-    #     self.sum9 = self.dataFrame["Austria"].sum()
-    #     self.sum10 = self.dataFrame["Scandinavia"].sum()
-    #     self.sum11 = self.dataFrame["CIS & Eastern Europe"].sum()
-    #
-    #     print(self.sum1)
-    #     print(self.sum2)
-    #     print(self.sum3)
-    #     print(self.sum4)
-    #     print(self.sum5)
-    #     print(self.sum6)
-    #     print(self.sum7)
-    #     print(self.sum8)
-    #     print(self.sum9)
-    #     print(self.sum10)
-    #     print(self.sum11)
-    #
-    #     print(f'The top 3 countries in Europe that visitors visit are United Kingdom {self.sum1}, Germany {self.sum2}, Netherlands {self.sum5}')
+    def TopThreeCountriesMean(self):
+        self.filter1 = self.dataFrame[2: 121]["United Kingdom"].mean()
+        self.filter2 = self.dataFrame[2: 121]["Germany"].mean()
+        self.filter3 = self.dataFrame[2: 121]["Netherlands"].mean()
+
+        # print(self.filter1)
+        # print(self.filter2)
+        #print(self.filter3)
+
+        print(f'The mean of United Kingdom is: {self.filter1}.')
+        print(f'The mean of Germany is: {self.filter2}.')
+        print(f'The mean of Netherlands is: {self.filter3}.')
 
 
-    # def medianOfTopThree(self):
-    #     self.median1 = self.dataFrame['United Kingdom'].median()
-    #     self.median2 = self.dataFrame['Germany'].median()
-    #     self.median3 = self.dataFrame['Netherlands'].median()
-    #
-    #     print(f'Median for the Top Three Countries is: {self.median1}.')
-    #     print(f'Median for the Top Three Countries is: {self.median2}.')
-    #     print(f'Median for the Top Three Countries is: {self.median3}.')
+    def TopThreeCountriesMedian(self):
+        self.filter4 = self.dataFrame[2: 121]["United Kingdom"].median()
+        self.filter5 = self.dataFrame[2: 121]["Germany"].median()
+        self.filter6 = self.dataFrame[2: 121]["Netherlands"].median()
 
-    # def meanOfTopThree(self):
-    #     self.mean1 = self.dataFrame['United Kingdom'].mean()
-    #     self.mean2 = self.dataFrame['Germany'].mean()
-    #     self.mean3 = self.dataFrame['Netherlands'].mean()
-    #
-    #     print(f'Mean for the Top Three Countries is: {self.mean1}.')
-    #     print(f'Mean for the Top Three Countries is: {self.mean2}.')
-    #     print(f'Mean for the Top Three Countries is: {self.mean3}.')
+        # print(self.filter4)
+        # print(self.filter5)
+        #print(self.filter6)
+
+        print(f'The median of United Kingdom is: {self.filter4}.')
+        print(f'The median of Germany is: {self.filter5}.')
+        print(f'The median of Netherlands is: {self.filter6}.')
 
 
+    def TotalVisitorUnitedKingdom(self):
+        self.filter7 = self.dataFrame[2: 121]["United Kingdom"].sum()
+        print(self.filter7)
+        return self.dataFrame[2: 121]["United Kingdom"].sum()
 
 
     def chart1(self):
@@ -133,18 +92,10 @@ class EuropeStatistics:
         plt.scatter(self.whatineed['United Kingdom'], self.whatineed['year'])
         plt.show();
 
+
     def chart2(self):
         self.ax = self.whatineed['Germany'].plot(kind='bar', title="Vistors", figsize=(10, 10), legend=True, fontsize=12)
         plt.show()
-
-
-
-
-        #plt.scatter(self.dataFrame['United Kingdom'], self.dataFrame['year'])
-        #plt.show();
-
-        #self.targetRows = self.dataFrame[self.dataFrame["Year"].str.contains(“1980”)]
-        #print(self.targetRows)
 
 
 europe = EuropeStatistics()
